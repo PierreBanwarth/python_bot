1c5d980f8a31ddfe0df7f01e1ff79dc6c15a93bc
pierre banwarth <pi.banwarth@gmail.com>
2018-11-20 11:58:31 +0100

    adding urllib

--- main.py
+++ main.py
@@ -7,18 +7,15 @@ from tinydb import TinyDB, Query
 import requests
 import sys
 import re
+import webModule
 
 PREFIX_URL = 'https://agendatrad.org/'
 contactList = []
 
 urlTab = {}
 def getPageListDetails(url, database):
-    page = requests.get(PREFIX_URL+url)
-    tree = html.fromstring(page.content)
-    href = tree.xpath('//a/@href')
 
-    href = list(set(href))
-
+    href = webModule.getAllLinks(PREFIX_URL+url)
     for item in href:
         print(item)
         if item.startswith('/orga_association/') or item.startswith('/orga_autre/'):
@@ -29,42 +26,32 @@ def getOrgaDetails(url, database):
     newContact = {}
     page = requests.get(PREFIX_URL+url)
     tree = html.fromstring(page.content)
-    # d1.decode("unicode_escape").encode('utf-8')
+
     newContact['name'] = tree.xpath('//h2[@class="entitie_name rouge"]/text()')[0]
     newContact['street-adress'] = tree.xpath('//span[@itemprop="street-address"]/text()')[0]
     newContact['postal-code'] =tree.xpath('//span[@itemprop="postal-code"]/text()')[0]
     newContact['locality'] = tree.xpath('//span[@itemprop="locality"]/text()')[0]
     newContact['country-name'] = tree.xpath('//span[@itemprop="country-name"]/text()')[0]
-    # <a href="http://www.12violoneux.blogspot.com" class="btn btn-small" target="_blank"><i class="fa fa-globe"></i> Site</a>
     # Verification du site de l'asso
     test = tree.xpath('//a[@class="btn btn-small"]/@href')
     if len(test)>0:
-        addressMailLit = []
         newContact['website'] = test[0]
         # Verification de l'adresse mail*
-        try:
-            newContactWebsite = requests.get(newContact['website'])
-            contactWebsiteTree = html.fromstring(newContactWebsite.content)
-            getMailTab = contactWebsiteTree.xpath('//*[contains(text(),"@")]/text()')
-            for text in getMailTab:
-                emails = re.findall(r'[\w\.-]+@[\w\.-]+', text)
-                for email in emails:
-                    if email.endswith('.com') or email.endswith('.fr') or email.endswith('.net'):
-                        print email
-                        addressMailLit.append(email)
-        except:
-            print('error while trying to reach website')
+        addressMailLit = webModule.getMailTabFromWebsite(newContact['website'])
         newContact['mail-address'] = addressMailLit
     # Verification des anciennes dates
     contact_url = PREFIX_URL+url[:-5]+'/oldEvent.html'
     page_contact = requests.get(contact_url)
     contact_tree = html.fromstring(page_contact.content)
     newContact['last-event-date'] = contact_tree.xpath('//*[contains(@class, "date_head")]/text()')
+    # Adding organisation to database if she has already make some events
     if len(newContact['last-event-date']) != 0:
         contactList.append(newContact)
         Orga = Query()
         if len(database.search(Orga.name == newContact['name'])) == 0:
             database.insert(newContact)
+        else:
+            print 'Organisation already recorded'
     # displayContact(newContact)
 
 def displayContact(item):
@@ -78,19 +65,23 @@ def displayContact(item):
     if item.has_key('mail-address'):
         print item['mail-address']
 
-def display(contactList):
-    for item in contactList:
-        displayContact(item)
-
-
+def getAllMail(database):
+    Orga = Query()
+    for item in database.search(Orga.website.exists()):
+        href = webModule.getAllLinks(item['website'])
+        print(href)
+        for link in href:
+            addressMailLit = webModule.getMailTabFromWebsite(link)
+            print addressMailLit
 
 def main():
+    # 
     db = TinyDB('db/database.json')
     table = db.table('Orga')
-    for i in range(1, 2):
-        # print('--'+str(i)+'--')
+
+    for i in range(1, 23):
         getPageListDetails('organisateurs/France?page='+str(i), table)
-    display(contactList)
+    getAllMail(table)
 
 if __name__ == "__main__":
     main()
---
+++ webModule.py
@@ -0,0 +1,59 @@
+# coding: utf-8
+import requests
+import urllib
+from lxml import html
+from urllib import urlopen
+
+import nltk
+from bs4 import BeautifulSoup
+import requests
+import requests.exceptions
+from urlparse import urlsplit
+from collections import deque
+import re
+
+
+
+
+def getAllLinks(url):
+    page = requests.get(url)
+    tree = html.fromstring(page.content)
+    href = tree.xpath('//a/@href')
+    return list(set(href))
+
+def getMailTabFromWebsite(url):
+    adressMailList = []
+    return crawlWebPage(url)
+    try:
+
+        newContactWebsite = requests.get(url)
+        contactWebsiteTree = html.fromstring(newContactWebsite.content)
+
+        getMailTab = contactWebsiteTree.xpath('//*[contains(text(),"@")]/text()')
+        print getMailTab
+        for text in getMailTab:
+            emails = re.findall(r'[\w\.-]+@[\w\.-]+', text)
+            for email in emails:
+
+                if email.endswith('.com') or email.endswith('.fr') or email.endswith('.net'):
+                    print email
+                    adressMailList.append(email)
+    except:
+        pass
+    return adressMailList + crawlWebPage(url)
+
+def crawlWebPage(url):
+    # a queue of urls to be crawled
+    new_emails = []
+    # extract base url to resolve relative links
+    parts = urlsplit(url)
+    base_url = "{0.scheme}://{0.netloc}".format(parts)
+    path = url[:url.rfind('/')+1] if '/' in parts.path else url
+    try:
+        response = requests.get(url)
+        new_emails = re.findall(r"[a-z0-9\.\-+_]+@[a-z0-9\.\-+_]+\.[a-z]+", response.text, re.I)
+
+    except (requests.exceptions.MissingSchema, requests.exceptions.ConnectionError):
+        # ignore pages with errors
+        pass
+    return new_emails
